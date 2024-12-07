21ffbed235bfebd3a383b8c57776ead1e6ab0f93
ornobaadi <aadi4789@gmail.com>
2024-12-07 16:40:18 +0600

    Env variables api key

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCcBBSAIaRHbxjiSLtnmJsyZR_r_79ltXE",
-    authDomain: "visa-navigator-4b126.firebaseapp.com",
-    projectId: "visa-navigator-4b126",
-    storageBucket: "visa-navigator-4b126.firebasestorage.app",
-    messagingSenderId: "996919540902",
-    appId: "1:996919540902:web:ef5aec93b1c1d6930e4867"
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
